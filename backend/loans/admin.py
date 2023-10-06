fd76614a521f5b72e8183a3c5e5cab4869e3cb5f
Lucas Wesley Fragoso <103780535+LucasWFragoso@users.noreply.github.com>
2023-10-06 12:49:15 -0300

    Revert "Dev"

--- backend/loans/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Client, TableTax, LoanSolicitation, Installment
-
-
-admin.site.register(Client)
-admin.site.register(TableTax)
--- frontend/loan-app/src/schemas/valueSchema.tsx
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const valueDataRequestSchema = z.object({
-    value: z.number().min(300).max(10000),
-});
-
-export type TValueDataRequest = z.infer<typeof valueDataRequestSchema>;
