a32f167184d2f52a12fff463ec6241bead283ad5
Lucas Wesley Fragoso <lucaswmmf@gmail.com>
2023-10-06 13:42:54 -0300

    fix:deploy settings railway backend

--- backend/loansystem/settings.py
+++ backend/loansystem/settings.py
@@ -14,6 +14,7 @@ from django.core.management.utils import get_random_secret_key
 import os
 import dj_database_url
 import dotenv
+from pathlib import Path
 
 dotenv.load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
